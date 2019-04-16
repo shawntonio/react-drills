aae769e8d8bf47cae5696d6a2bd575131c5781d3
Shawn Turley <antonioturley@gmail.com>
2019-04-16 16:32:07 -0600

    finished 8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      people: []
+    }
+  }
+  
+  componentDidMount(){
+    axios.get("https://swapi.co/api/people/").then(res => {
+      this.setState({people: res.data.results})
+      console.log(this.people)
+    }).catch(err => console.log('err', err))
+    
+  }
+  
   render() {
+    let id = 1
+    const {people} = this.state
+    const person = people.map(person => (
+      <h3 key={id++} >{person.name}</h3>
+    ))
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         {person}
       </div>
     );
   }
